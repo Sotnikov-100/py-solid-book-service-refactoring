33568ddda2a0ba7d520751bf774fac8d207ffc7c
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-05-23 09:08:37 +0300

    Fix flake8

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 
 class Book:
-    def __init__(self, title: str, content: str):
+    def __init__(self, title: str, content: str) -> None:
         self.title = title
         self.content = content
 
